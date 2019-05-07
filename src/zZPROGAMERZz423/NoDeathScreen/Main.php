844488f0e6cb5566047cc11a3e0b73e143710517
ZayDepths <40534566+zZPROGAMERZz423@users.noreply.github.com>
2019-05-08 00:29:58 +0800

    Update Main.php

--- src/zZPROGAMERZz423/NoDeathScreen/Main.php
+++ src/zZPROGAMERZz423/NoDeathScreen/Main.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace zZPROGAMERZz423\NoDeathScreen;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+    
+    public function onDamage(EntityDamageEvent $event) {
+$player = $event->getEntity();
+if($event->getFinalDamage() >= $player->getHealth()) {
+$event->setCancelled();
+$player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+$player->setHealth($player->getMaxHealth());
+$player->sendMessage("§cYou died");
+   }
+}
+
+    public function onDisable(){
+    }
+}
